a0355ddef1e7a47b6f406fd2ca1100a0a1e23d93
Assem <assem99.hossam@gmail.com>
2022-03-30 17:36:07 +0200

    added a file that displays error if there is no entry or displays the entry in markup lang. if it is existed

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 from . import util
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def displayEntry(request, name):
+    entryText = str(util.get_entry(name))
+    # return HttpResponse(entryText)
+    return render(request, 'encyclopedia/entry.html', {
+        'title' : name,
+        'entryText' : entryText
     })
 

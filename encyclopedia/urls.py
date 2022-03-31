aa09376e190f627c458244408645f23c126531de
Assem <assem99.hossam@gmail.com>
2022-03-31 16:55:25 +0200

    editing entries part is finished

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
     path('searchResults', views.displaySearchResults ,name='searchResults'),
     path('new', views.createEntry, name='newEntry'),
+    path('edit/<str:name>', views.editPage, name='editPage'),
     path('<str:name>', views.displayEntry, name='entryName')
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -11,6 +11,9 @@ class newEntryForm(forms.Form):
 class SearchForm(forms.Form):
     search = forms.CharField(widget= forms.TextInput (attrs={'placeholder':'Search Encyclpedia'}))
 
+class editForm(forms.Form):
+    entryText = forms.CharField(widget=forms.Textarea(attrs={'rows':'3', 'cols':'5'}), label='Content')
+
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(),
@@ -59,7 +62,9 @@ def createEntry(request):
             entryTitle = form.cleaned_data['entryTitle']
             entryText = form.cleaned_data['entryText']
             allEntries = util.list_entries()
-            if entryTitle in allEntries:
+            for i in range(0, len(allEntries)):
+                allEntries[i] = allEntries[i].lower()
+            if entryTitle.lower() in allEntries:
                 return render(request, 'encyclopedia/create.html', {
                     'form' : SearchForm(),
                     'entryForm' : form,
@@ -79,4 +84,22 @@ def createEntry(request):
         'form' : SearchForm(),
         'entryForm' : newEntryForm(),
         'entryTitle' : entryTitle
+    })
+
+def editPage(request, name):
+    content = util.get_entry(name)
+    dic = {'entryText' : content}
+    if request.method == 'POST':
+        form = editForm(request.POST)
+        if form.is_valid():
+            newContent = form.cleaned_data['entryText']
+            util.save_entry(name, newContent)
+            return HttpResponseRedirect(reverse('entryName', args=[name]))
+        else:
+            return render(request, 'encyclopedia/index.html')
+    return render(request, 'encyclopedia/edit.html', {
+        'form' : SearchForm(),
+        'entryName' : name,
+        'editForm' : editForm(dic),
+        'content' : content
     })
